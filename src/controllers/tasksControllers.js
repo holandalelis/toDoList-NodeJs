7106a8e2ee5d09b833c69aedd2e2bee27bf9a17d
pedro <pedrolelis@hotmail.com>
2023-03-25 17:52:15 -0300

    feat: conexão com banco, controllers, etc.

---
+++ src/controllers/tasksControllers.js
@@ -0,0 +1,9 @@
+const getAll = async () => {
+  const tasks = await connection.execute('SELECT * FROM tasks');
+
+  return tasks;
+}
+
+module.exports = {
+    getAll,
+};
--- src/router.js
+++ src/router.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => { res.status(200).send('Hello World!'); });
+const tasksController = require('../controllers/tasksController');
+
+
+router.get('/tasks', tasksController.getAll);
 
 module.exportes = router;
